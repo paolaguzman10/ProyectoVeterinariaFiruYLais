4331eac930f9db7694169a067385e711ad0dab79
josskato <157661167+josskato@users.noreply.github.com>
2026-06-08 19:27:56 -0600

    prueba de git

--- src/main/java/com/mycompany/proyectoveterinariafiruylais/ProyectoVeterinariaFiruYLais.java
+++ src/main/java/com/mycompany/proyectoveterinariafiruylais/ProyectoVeterinariaFiruYLais.java
@@ -11,6 +11,6 @@ package com.mycompany.proyectoveterinariafiruylais;
 public class ProyectoVeterinariaFiruYLais {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");//Prueba del uso de github desktop
     }
 }
